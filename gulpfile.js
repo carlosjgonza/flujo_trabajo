546681dd4d31a5ec1af3fa008b03db82bb2692bd
Carlos González <carlosj.gonzalez28@gmail.com>
2014-07-20 17:06:10 -0430

    Concatenando archivos javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 
 var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat'),
 	gutil = require('gulp-util');
 
 var coffeeSources = ['componentes/coffee/*.coffee'];
+
+var jsSources = [
+	'componentes/scripts/pixgrid.js',
+	'componentes/scripts/rclick.js',
+	'componentes/scripts/tagline.js',
+	'componentes/scripts/template.js'
+];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
 	.pipe(coffee({bare:true})
 	.on('error', gutil.log))
 	.pipe(gulp.dest('componentes/scripts'));
+});
+
+gulp.task('js', function(){
+
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/desarrollo/js'));
+
 });
